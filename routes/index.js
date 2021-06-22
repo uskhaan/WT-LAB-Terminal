6f3eaf85430f4d249cc8332df40015d7d968dee8
Umar Shahbaz Khan <umar.sk179@gmail.com>
2021-06-22 13:17:15 +0500

    first-commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var router = express.Router();
+var Product = require("../models/product");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", async function (req, res, next) {
+  let products = await Product.find();
+  console.log(req.session.user);
+  console.log("products: ", products);
+  res.render("products/list", { title: "Products In DB", products });
 });
+
+// /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
 router.get("/cart", function (req, res, next) {
   let cart = req.cookies.cart;
   if (!cart) cart = [];
