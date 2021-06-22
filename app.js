1066335c51f143ce4eabd8a5e247ead2549bd4d3
Umar Shahbaz Khan <umar.sk179@gmail.com>
2021-06-22 11:28:18 +0500

    first-commit

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect("mongodb://localhost/productscrud", {
+  .connect("mongodb+srv://gasolel:gasolel@cluster0.i0r77.mongodb.net/DevProduct?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
